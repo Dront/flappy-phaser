97fdcc430c165b72472b74d766bddde56ef5cfd0
drontosaur <drontosaur@yandex-team.ru>
2021-05-13 09:29:08 +0300

    jumping circle

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import Phaser from 'phaser';
 class MyGame extends Phaser.Scene {
     constructor() {
         super();
+        this.maxSpeed = 300;
     }
 
-    create () {
-        let circle = this.add.circle(400, 300, 100, 0xff0088);
-        this.tweens.add({
-            targets: circle,
-            scaleX: 0.5,
-            scaleY: 0.5,
-            yoyo: true,
-            repeat: -1,
-            ease: 'Sine.easeInOut'
-        });
+    create() {
+        this.player = this.add.circle(400, 300, 50, 0xff8800);
+        this.physics.add.existing(this.player);
+        this.player.body.collideWorldBounds = true;
+        this.player.body.setBounce(0.2);
+        this.player.body.velocity.y = -100;
+        this.input.on("pointerdown", this.jump, this);
+    }
+
+    jump() {
+        const curSpeed = this.player.body.velocity.y;
+        this.player.body.velocity.y = Math.max(-this.maxSpeed, curSpeed - 100);
     }
 }
 
@@ -23,7 +26,13 @@ const config = {
     width: 800,
     height: 600,
     backgroundColor: "#5DACD8",
-    scene: MyGame
+    scene: MyGame,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 100 },
+        },
+    },
 };
 
 const game = new Phaser.Game(config);
