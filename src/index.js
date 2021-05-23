d960e6398eeea893e89dca3306c3093771c46cb0
drontosaur <drontosaur@yandex-team.ru>
2021-05-23 13:20:32 +0300

    scale game to fit screen

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
     // this makes rendered text objects look better (by x2 amount of rendering needed)
     resolution: window.devicePixelRatio,
     backgroundColor: "#5DACD8",
