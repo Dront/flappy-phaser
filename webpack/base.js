e0d55c80e1f7ef1d78839665330c0481b711e877
drontosaur <drontosaur@yandex-team.ru>
2021-08-25 00:35:11 +0300

    Revert "include assets in build process"
    This reverts commit 22b061b21cfc00475b9fba2611f37c5e5c6d5046.

--- webpack/base.js
+++ webpack/base.js
@@ -2,8 +2,6 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-
 
 module.exports = {
   mode: "development",
@@ -37,11 +35,6 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: "./index.html"
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "assets", to: "assets" },
-      ],
-    }),
+    })
   ]
 };
