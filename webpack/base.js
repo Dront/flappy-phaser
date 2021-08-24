22b061b21cfc00475b9fba2611f37c5e5c6d5046
drontosaur <drontosaur@yandex-team.ru>
2021-08-25 00:06:40 +0300

    include assets in build process

--- webpack/base.js
+++ webpack/base.js
@@ -2,6 +2,8 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
 
 module.exports = {
   mode: "development",
@@ -35,6 +37,11 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: "./index.html"
-    })
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "assets", to: "assets" },
+      ],
+    }),
   ]
 };
